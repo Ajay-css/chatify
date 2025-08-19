daf07a66546454b40d582cb1d2e37c75cd4d52c7
ajayarumugam73@gmail.com <you@example.com>
2025-08-19 22:52:45 +0530

    url updated in backend

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -7,7 +7,7 @@ let io;
 export function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: "https://ajay-chat.onrender.com/", // ✅ allow only your frontend
+      origin: "https://ajay-chat.onrender.com", // ✅ allow only your frontend
       credentials: true,
     },
   });
