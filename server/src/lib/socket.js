c6d98e31581fe14e65b9001ee19de0df8e79f323
ajayarumugam73@gmail.com <you@example.com>
2025-08-19 22:42:27 +0530

    frontend url updated

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -7,7 +7,7 @@ let io;
 export function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173", // ✅ allow only your frontend
+      origin: "https://ajay-chat.onrender.com/", // ✅ allow only your frontend
       credentials: true,
     },
   });
