67ef6bc0fe429ab7917e90133252c4ef5f38fe42
ajayarumugam73@gmail.com <you@example.com>
2025-08-20 04:55:32 +0530

    file size changed

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,8 @@ const server = createServer(app); // 👈 create server once
 
 await connectDb();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));  
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));  
 app.use(cookieParser());
 app.use(
     cors({
