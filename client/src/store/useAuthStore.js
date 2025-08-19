795e9f44e8e66cb8be12f80353a7428cb7732125
ajayarumugam73@gmail.com <you@example.com>
2025-08-19 22:36:32 +0530

    backend new url uploaded

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import { axiosInstance } from "../lib/axios.js";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://chatify-backend-zowh.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
